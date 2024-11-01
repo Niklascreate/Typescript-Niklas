d9f5f577430fca1a582e75e35679f115d0c95a1d
Adam Rehal <Niklasn89@gmail.com>
2024-11-01 12:50:34 +0100

    Joke app in progress

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Typescript-Niklas
 Indivuduell uppgift i typecript.
+
+
+
+Random joke:  GET https://api.humorapi.com/jokes/random
+
+Random Meme: GET https://api.humorapi.com/memes/random
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+let buttonRef: HTMLButtonElement = document.querySelector('#randomizeButton') as HTMLButtonElement;
+
+buttonRef?.addEventListener('click', () => {
+    fetchRandomJoke();
+});
+
+async function fetchRandomJoke(): Promise<void> {
+    const apiUrl: string = 'https://api.humorapi.com/jokes/random';
+    const apiKey: string = 'c622bc16f7a44bea9ec5b1b5f82431ac';
+
+    try {
+        const response: Response = await fetch(`${apiUrl}?key=${apiKey}`);
+        if (!response.ok) {
+            throw new Error('Someting went wrong, could not fetch the joke.');
+        }
+        const data: { joke: string } = await response.json();
+        const contentRef: HTMLElement | null = document.querySelector('#giggleContent');
+        if (contentRef) {
+            contentRef.innerText = data.joke;
+        }
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.error('Wrong:', error.message);
+        } else {
+            console.error('Unknown error discovered');
+        }
+    }
+}
